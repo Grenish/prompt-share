43bd30fe9407c50e2814b156498c0827951bfdca
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-09-16 16:06:53 +0530

    feat: enhance DashboardLayout with cookie-based sidebar state management and Toaster integration

--- app/(afterAuth)/dashboard/layout.tsx
+++ app/(afterAuth)/dashboard/layout.tsx
@@ -1,18 +1,22 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { Toaster } from "@/components/ui/sonner";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const sidebarCookie = cookieStore.get("sidebar_state")?.value;
+  const defaultOpen = sidebarCookie ? sidebarCookie === "true" : true;
+
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <main>{children}</main>
+      <Toaster position="top-right" richColors closeButton/>
     </SidebarProvider>
   );
 }
