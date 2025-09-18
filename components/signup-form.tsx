b1e34642ccf90b6bfa3d681c5d3b5f53e43f3d8d
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-09-19 00:53:06 +0530

    fix: update import path for signup action to authActions

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { signup } from "@/util/actions";
+import { signup } from "@/util/actions/authActions";
 
 export function SignupForm({
   className,
