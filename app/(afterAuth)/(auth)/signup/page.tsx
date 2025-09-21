e7e897c8a5182d6aa92fbf63cc83880efd535ea0
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-09-22 00:31:08 +0530

    feat: update SignupPage to use correct image source and enhance layout

--- app/(afterAuth)/(auth)/signup/page.tsx
+++ app/(afterAuth)/(auth)/signup/page.tsx
@@ -1,14 +1,16 @@
+import DisplayPromptBox from "@/components/display-prompt-box";
 import { SignupForm } from "@/components/signup-form";
 
 export default function SignupPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="bg-muted relative hidden lg:block">
+      <div className="relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="/signup.png"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover"
         />
+        <DisplayPromptBox className="absolute p-3 border border-foreground/20 rounded-xl backdrop-blur-sm bg-background/5 bottom-24 left-1/2 transform -translate-x-1/2 " />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
