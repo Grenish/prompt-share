e42e6a005faa601c53af082970e6a5d8e4b2252e
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-09-23 22:09:08 +0530

    feat: enhance empty state display for user posts with improved messaging and visuals

--- app/(afterAuth)/home/profile/page.tsx
+++ app/(afterAuth)/home/profile/page.tsx
@@ -214,11 +214,23 @@ export default async function DashboardProfilePage() {
                   // }}
                 />
               ) : (
-                <div className="py-12 sm:py-16 text-center">
-                  <div className="w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-muted mx-auto mb-3 sm:mb-4" />
-                  <p className="text-muted-foreground text-xs sm:text-sm">
-                    No posts yet
-                  </p>
+                <div className="mx-auto text-sm flex flex-col items-center justify-center text-muted-foreground pointer-events-none select-none">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="150"
+                    height="150"
+                    fill="currentColor"
+                    viewBox="0 0 256 256"
+                  >
+                    <path
+                      d="M224,128a96,96,0,1,1-96-96A96,96,0,0,1,224,128Z"
+                      opacity="0.2"
+                    ></path>
+                    <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216ZM80,108a12,12,0,1,1,12,12A12,12,0,0,1,80,108Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,176,108Zm-1.08,64a8,8,0,1,1-13.84,8c-7.47-12.91-19.21-20-33.08-20s-25.61,7.1-33.08,20a8,8,0,1,1-13.84-8c10.29-17.79,27.39-28,46.92-28S164.63,154.2,174.92,172Z"></path>
+                  </svg>
+                  <span>
+                    <h2>You haven't posted anything yet.</h2>
+                  </span>
                 </div>
               )}
             </TabsContent>
