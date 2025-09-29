bc598a074c3bc752fbf83a24e529eeb96cad2294
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-09-29 15:58:26 +0530

    Add Privacy Policy page component

---
+++ app/(home)/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicy from "@/markdown/privacy-policy.mdx";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <div className="mx-auto w-full max-w-3xl px-4 pb-16 pt-6 sm:px-6 mt-20">
+      <PrivacyPolicy />
+    </div>
+  );
+}
